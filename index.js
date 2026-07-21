8c1380ec634e6a5a622d9b63f2cfbdb3e76e5324
sabbirRashed <srsabbirrahman12@gmail.com>
2026-07-21 21:50:44 +0600

    successfully added express to mongodb

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+const run = async () => {
+    try {
+
+        await client.connect();
+
+        const db = client.db('SparkNest');
+        const ideasCollection = db.collection('ideas');
+
+        // API
+        app.get('/ideas', async (req, res) => {
+            const result = await ideasCollection.find().toArray();
+            res.send(result);
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', async (req, res) => {
+    res.send('Hello from express server')
+})
+
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
